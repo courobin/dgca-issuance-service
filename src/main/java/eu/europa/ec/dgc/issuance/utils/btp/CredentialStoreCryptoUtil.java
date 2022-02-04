2223058a5dc561ecf36c3c952db1dd8f23177993
courobin <92510632+courobin@users.noreply.github.com>
2022-02-04 12:59:11 +0100

    Update CredentialStoreCryptoUtil.java

--- src/main/java/eu/europa/ec/dgc/issuance/utils/btp/CredentialStoreCryptoUtil.java
+++ src/main/java/eu/europa/ec/dgc/issuance/utils/btp/CredentialStoreCryptoUtil.java
@@ -1,6 +1,7 @@
 package eu.europa.ec.dgc.issuance.utils.btp;
 
 import com.google.gson.Gson;
+import org.json.simple.JSONObject;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWEObject;
 import com.nimbusds.jose.Payload;
